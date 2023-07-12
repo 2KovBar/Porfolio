564b5a07e9e489db1525c7aeba23a4c940321d2c
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2023-07-12 19:00:50 +0300

    fix logo size

--- components/Header.js
+++ components/Header.js
@@ -7,14 +7,14 @@ import Socials from "../components/Socials";
 
 const Header = () => {
   return (
-    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[90px]">
+    <header className="absolute z-30 w-full flex items-center px-16 xl:px-0 xl:h-[110px]">
       <div className="container mx-auto ">
         <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
           <Link href={"/"}>
             <Image
               src={"/2KB_1-removebg-preview (1).png"}
-              width={220}
-              height={48}
+              width={180}
+              height={180}
               alt=""
               priority={true}
             />
